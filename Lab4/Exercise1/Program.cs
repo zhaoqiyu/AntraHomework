07cd911ba7f621a66f870b4f6552a1b18e55aca4
Qiyu Zhao <zhaoqiyu1992@gmail.com>
2015-08-19 00:32:33 -0400

    Lab4 Ver0.1

---
+++ Lab4/Exercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NewLanguageFeatures
+{
+    public class Customer
+    {
+        public int CustomerID { get; private set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public Customer(int ID) {
+            CustomerID = ID;
+        }
+
+        public override string ToString() {
+            return Name + "\t" + City + "\t" + CustomerID;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            Customer c = new Customer(1);
+            c.Name = "Maria Anders";
+            c.City = "Berlin";
+  
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Lab4/Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        Point p = new Point { X = 3, Y = 99 };
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            Customer c = new Customer(1) { Name = "Maria Anders", City = "Berlin" };
+            Console.WriteLine(c);
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
