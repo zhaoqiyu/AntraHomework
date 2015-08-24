d4308400cf977988c458a2172884702a2d35a272
Qiyu Zhao <zhaoqiyu1992@gmail.com>
2015-08-24 00:48:05 -0400

    123123
    qrweqrw

---
+++ RegistrationFrom/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SignUp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+    protected void btnCreateAccount_Click(object sender, EventArgs e) {
+        ClientScript.RegisterStartupScript(GetType(), "onClick", "<script>alert('account created');</script>");
+    }
+}
